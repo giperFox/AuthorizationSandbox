73db639f366b3da4fd5caafa6a882e07ce897886
Oleksandr Kasatkin <kasatkin.alexander@gmail.com>
2018-05-06 13:08:43 +0300

    Test of the Bamboo tracker 1

--- QuickstartAPI/Controllers/IdentityController.cs
+++ QuickstartAPI/Controllers/IdentityController.cs
@@ -4,15 +4,22 @@ using System.Linq;
 
 namespace QuickstartAPI.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/Identity")]
-    [Authorize]
-    public class IdentityController : Controller
-    {
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
-        }
-    }
+	[Produces("application/json")]
+	[Route("api/Identity")]
+	[Authorize]
+	public class IdentityController : Controller
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+		}
+
+		[HttpGet]
+		[Route("test")]
+		public IActionResult Test()
+		{
+			return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+		}
+	}
 }
