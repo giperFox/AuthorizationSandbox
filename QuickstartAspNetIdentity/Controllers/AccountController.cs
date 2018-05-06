9bc4899e8f622c2b3aecd77e7a795acd1ea8d231
Oleksandr Kasatkin <kasatkin.alexander@gmail.com>
2018-05-06 13:12:03 +0300

    Bamboo tracker test 2

--- QuickstartAspNetIdentity/Controllers/AccountController.cs
+++ QuickstartAspNetIdentity/Controllers/AccountController.cs
@@ -41,9 +41,23 @@ namespace QuickstartAspNetIdentity.Controllers
             _logger = loggerFactory.CreateLogger<AccountController>();
         }
 
-        //
-        // GET: /Account/Login
-        [HttpGet]
+		//
+		// GET: /Account/Login
+		[HttpGet]
+		[AllowAnonymous]
+		[Route("test")]
+		public async Task<IActionResult> Login(string returnUrl = null)
+		{
+			// Clear the existing external cookie to ensure a clean login process
+			await HttpContext.Authentication.SignOutAsync(_externalCookieScheme);
+
+			ViewData["ReturnUrl"] = returnUrl;
+			return View();
+		}
+
+		//
+		// GET: /Account/Login
+		[HttpGet]
         [AllowAnonymous]
         public async Task<IActionResult> Login(string returnUrl = null)
         {
